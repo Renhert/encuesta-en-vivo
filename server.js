e36091915b1cf9599b946dec1888d0009d49dec6
Renhert <victor123@gmail.com>
2025-04-28 09:18:55 +0200

    Mover server.js a raíz

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, 0.0.0.0, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
